f45795f3ea459589ac10055e0f6c9d0ab3edf40d
Davi Ribeiro <davirpa1@gmail.com>
2023-05-25 10:52:10 -0300

    printing selected piece possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -28,16 +28,34 @@ public class UI {
         for (int i = 0; i < pieces.length; i++){
             System.out.print((8 - i) + " ");
             for(int j = 0; j < pieces.length; j++){
-                printPiece(pieces[i][j]);
+                printPiece(pieces[i][j], false);
             }
             System.out.println();
         }
         System.out.println("  a b c d e f g h");
     }
 
-    private static void printPiece(ChessPiece piece){
+    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+        for (int i = 0; i < pieces.length; i++){
+            System.out.print((8 - i) + " ");
+            for(int j = 0; j < pieces.length; j++){
+                printPiece(pieces[i][j], possibleMoves[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("  a b c d e f g h");
+    }
+
+    private static void printPiece(ChessPiece piece, boolean background){
+        String emptyPosition;
+        if(background){
+            emptyPosition = "+ ";
+        }
+        else {
+            emptyPosition = "- ";
+        }
         if (piece == null){
-            System.out.print("- ");
+            System.out.print(emptyPosition);
         }
         else {
             System.out.print(piece + " ");
